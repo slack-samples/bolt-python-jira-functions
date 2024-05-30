58e684503ffeee540c7317543dcd542def4b93bb
William Bergamin <wbergamin@slack-corp.com>
2024-05-30 12:14:47 -0400

    Add comments

--- jira/oauth/installation_store/models.py
+++ jira/oauth/installation_store/models.py
@@ -8,6 +8,7 @@ class JiraInstallation(TypedDict):
     expires_in: int
     refresh_token: str
     user_id: str
+    # Either team_id or enterprise_id must exist here
     team_id: Optional[str]
     enterprise_id: Optional[str]
     installed_at: float
--- jira/oauth/state_store/models.py
+++ jira/oauth/state_store/models.py
@@ -5,5 +5,6 @@ class JiraUserIdentity(TypedDict):
     """Class for keeping track of individual slack users"""
 
     user_id: str
+    # Either team_id or enterprise_id must exist here
     team_id: Optional[str]
     enterprise_id: Optional[str]
